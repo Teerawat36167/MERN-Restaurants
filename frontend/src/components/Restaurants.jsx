51639cd009f22dc78408bc262d015d4253803ce9
Teerawat Chanvipardilok <82800595+Teerawat36167@users.noreply.github.com>
2023-01-03 14:07:43 +0700

    Fix check condition user and review name

--- frontend/src/components/Restaurants.jsx
+++ frontend/src/components/Restaurants.jsx
@@ -72,7 +72,7 @@ const Restaurants = (props) => {
                                     <strong>User: </strong>{review.name}<br/>
                                     <strong>Date: </strong>{review.date}
                                 </p>
-                                {props.user && props.user.id === review.user_id &&
+                                {props.user && props.user.id === review.user_id && props.user.name === review.name &&
                                     <div className="row">
                                         <a onClick={() => deleteReview(review._id, index)} className="btn btn-primary col-lg-5 mx-1 mb-1">Delete</a>
                                         <Link to={"/restaurants/" + id + "/review"} state={{currentReview: review}} className="btn btn-primary col-lg-5 mx-1 mb-1">Edit</Link>
