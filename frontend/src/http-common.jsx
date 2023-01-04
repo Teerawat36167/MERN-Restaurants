d016f133894f1a8d2bee616bdcf4b65f0893d6b9
Teerawat Chanvipardilok <82800595+Teerawat36167@users.noreply.github.com>
2023-01-04 18:03:47 +0700

    Update baseURL

--- frontend/src/http-common.jsx
+++ frontend/src/http-common.jsx
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "http://localhost:3005/api/v1/restaurants",
+    // baseURL: "http://localhost:3005/api/v1/restaurants", //localhost
+    baseURL: "https://restaurant-review-backend.vercel.app/api/v1/restaurants",
     headers: {
         "Content-type": "application/json"
     }
